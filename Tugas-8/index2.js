975f6441b1c1deea1ba6b210fd826ced9f90a35d
reader00 <ilya3anggela@gmail.com>
2020-09-02 13:55:48 +0700

    Task 8: JavaScript - Asynchronous

---
+++ Tugas-8/index2.js
@@ -0,0 +1,14 @@
+var readBooksPromise = require("./promise.js");
+
+var books = [
+  { name: "LOTR", timeSpent: 3000 },
+  { name: "Fidas", timeSpent: 2000 },
+  { name: "Kalkulus", timeSpent: 4000 },
+  { name: "komik", timeSpent: 1000 },
+];
+
+readBooksPromise(10000, books[0])
+  .then((time) => readBooksPromise(time, books[1]))
+  .then((time) => readBooksPromise(time, books[2]))
+  .then((time) => readBooksPromise(time, books[3]))
+  .catch((time) => `Sisa waktu: ${time}`);
